34c7de046ae6667420d3a32b7869370e373cd424
Vallabhbhagat <bhagatvallabh863@gmail.com>
2025-09-02 22:09:44 +0530

    add cart functionality

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../css/Home.css"
 
 const Home = () => {
@@ -7,7 +8,9 @@ const Home = () => {
         <div className="hero-text">
           <h2>Discover Premium Wrist Watches</h2>
           <p>Elevate your style with our exclusive collection</p>
-          <a href="/Products"><button>Shop Now</button></a>
+          <Link to="/Products">
+            <button>Shop Now</button>
+          </Link>
         </div>
         <img
           src="https://imgs.search.brave.com/Wt4rT_vNS8to3U58AXkLdbLTw2IBEPn8e94QWmogmKE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTA2/NTMyOTA1OC9waG90/by9wcmVtaXVtLW1l/bnMtd2F0Y2gtb24t/aGFuZC1jbG9zZS11/cC5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9VmRwVWFjOE9i/ckRaWjBJOE9DRF92/T3hQcXlPVUNhM2NN/OHpvcVhzUGt1RT0"
@@ -32,8 +35,8 @@ const Home = () => {
       </section>
       <div className="bar">
         <footer className="footer">
-        <p>&copy; {new Date().getFullYear()} WatchStore — All rights reserved.</p>
-      </footer></div>
+          <p>&copy; {new Date().getFullYear()} WatchStore — All rights reserved.</p>
+        </footer></div>
 
     </div>
   )
